94ab95128b7d5455c6b3af68f5c3a21e33501c25
esantos2019254 <esantos-2019254@kinal.edu.gt>
2024-01-22 08:35:43 -0600

    Agregamos logicaAsincrona y modificamos script en index

---
+++ assets/logicaAsincrona.js
@@ -0,0 +1,32 @@
+async function enviarDatos() {
+    const nombre = document.getElementById('nombre').value;
+
+    try {
+        const resultado = await enviarDatosAlServidor(nombre);
+        mensajeAgradecimiento(resultado);
+    } catch (e) {
+        mensajeError(e);
+    }
+}
+
+function enviarDatosAlServidor(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const exito = nombre.toLowerCase() !== 'error';
+            const resulado = exito ? "Bienvenido al sistema" : "Acceso denegado";
+            exito ? resolve(resulado) : reject(resulado);
+        }, 2000);
+    });
+}
+
+function mensajeAgradecimiento(mensaje) {
+    const mensajeElement = document.getElementById('mensaje');
+    mensajeElement.textContent = mensaje;
+    mensajeElement.classList.remove('oculto');
+}
+
+function mensajeError(error) {
+    const mensajeElement = document.getElementById('mensaje');
+    mensajeElement.textContent = error;
+    mensajeElement.classList.remove('oculto');
+}
